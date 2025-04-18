064f4cebb27b50bb530195d9c06e272edaf012b7
Raden Wachyu <rmuhammadwachyu@gmail.com>
2025-04-18 21:26:43 +0700

    Add files via upload

---
+++ Email List/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Email_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtNama = new TextBox();
+            txtEmail = new TextBox();
+            btnSimpan = new Button();
+            dataGridView1 = new DataGridView();
+            btnEdit = new Button();
+            btnHapus = new Button();
+            btnLoad = new Button();
+            label4 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F);
+            label1.Location = new Point(43, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(140, 41);
+            label1.TabIndex = 0;
+            label1.Text = "Email List";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(43, 113);
+            label2.Name = "label2";
+            label2.Size = new Size(49, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Nama";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(46, 186);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Email";
+            // 
+            // txtNama
+            // 
+            txtNama.Location = new Point(46, 136);
+            txtNama.Name = "txtNama";
+            txtNama.Size = new Size(220, 27);
+            txtNama.TabIndex = 3;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(46, 209);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(220, 27);
+            txtEmail.TabIndex = 4;
+            // 
+            // btnSimpan
+            // 
+            btnSimpan.Location = new Point(172, 256);
+            btnSimpan.Name = "btnSimpan";
+            btnSimpan.Size = new Size(94, 29);
+            btnSimpan.TabIndex = 5;
+            btnSimpan.Text = "Simpan";
+            btnSimpan.UseVisualStyleBackColor = true;
+            btnSimpan.Click += btnSimpan_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(311, 97);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(550, 188);
+            dataGridView1.TabIndex = 6;
+            dataGridView1.SelectionChanged += dataGridView1_SelectionChanged;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(311, 306);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(94, 29);
+            btnEdit.TabIndex = 7;
+            btnEdit.Text = "Edit";
+            btnEdit.UseVisualStyleBackColor = true;
+            btnEdit.Click += btnEdit_Click;
+            // 
+            // btnHapus
+            // 
+            btnHapus.Location = new Point(429, 306);
+            btnHapus.Name = "btnHapus";
+            btnHapus.Size = new Size(94, 29);
+            btnHapus.TabIndex = 8;
+            btnHapus.Text = "Hapus";
+            btnHapus.UseVisualStyleBackColor = true;
+            btnHapus.Click += btnHapus_Click;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(552, 306);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(94, 29);
+            btnLoad.TabIndex = 9;
+            btnLoad.Text = "Load";
+            btnLoad.UseVisualStyleBackColor = true;
+            btnLoad.Click += btnLoad_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(311, 60);
+            label4.Name = "label4";
+            label4.Size = new Size(335, 20);
+            label4.TabIndex = 10;
+            label4.Text = "Pilih Baris Untuk Mengedit Atau Menghapus Data";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(915, 384);
+            Controls.Add(label4);
+            Controls.Add(btnLoad);
+            Controls.Add(btnHapus);
+            Controls.Add(btnEdit);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnSimpan);
+            Controls.Add(txtEmail);
+            Controls.Add(txtNama);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Email List";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtNama;
+        private TextBox txtEmail;
+        private Button btnSimpan;
+        private DataGridView dataGridView1;
+        private Button btnEdit;
+        private Button btnHapus;
+        private Button btnLoad;
+        private Label label4;
+    }
+}
